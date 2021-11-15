84fca9fda3ce07ee83e3247818c4600652a7ca98
Siddharth <jainnsidhearth77@gmail.com>
2021-11-16 04:11:55 +0530

    save and fetch data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,24 +7,31 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 @Injectable()
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>();
-    private recipes: Recipe[] = [
-        new Recipe('Recipe Name',
-        'Recipe Description',
-        'https://static01.nyt.com/images/2021/06/01/dining/11lightveg-roundup-promo/11lightveg-roundup-8-superJumbo-v2.jpg?quality=75&auto=webp',
-        [
-            new Ingredient('Paneer',2),
-            new Ingredient('Sauce',1),
-        ]),
-        new Recipe('Another Recipe Name',
-        'Another Recipe Description',
-        'https://static01.nyt.com/images/2021/06/01/dining/11lightveg-roundup-promo/11lightveg-roundup-8-superJumbo-v2.jpg?quality=75&auto=webp',
-        [
-            new Ingredient('Green chilli',20),
-            new Ingredient('Red Sauce',1),
-        ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('Recipe Name',
+    //     'Recipe Description',
+    //     'https://static01.nyt.com/images/2021/06/01/dining/11lightveg-roundup-promo/11lightveg-roundup-8-superJumbo-v2.jpg?quality=75&auto=webp',
+    //     [
+    //         new Ingredient('Paneer',2),
+    //         new Ingredient('Sauce',1),
+    //     ]),
+    //     new Recipe('Another Recipe Name',
+    //     'Another Recipe Description',
+    //     'https://static01.nyt.com/images/2021/06/01/dining/11lightveg-roundup-promo/11lightveg-roundup-8-superJumbo-v2.jpg?quality=75&auto=webp',
+    //     [
+    //         new Ingredient('Green chilli',20),
+    //         new Ingredient('Red Sauce',1),
+    //     ])
+    // ];
+    private recipes : Recipe[] =[];
     constructor(private shoppingListService: ShoppingListService){
     }
+
+    setRecipes(recipes: Recipe[]){
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
+    
     getRecipes(){
         return this.recipes.slice();
     }
